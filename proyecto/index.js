53ae336665d0c93847708a78dad4d02e4463e952
Lizandro Antonio <codeversebackend@gmail.com>
2024-02-05 20:18:06 -0600

    Finish proyect with library Readline of Node.js, proyect of tasks

--- proyecto/index.js
+++ proyecto/index.js
@@ -1,4 +1,8 @@
-const readline = require('readline'); 
+const readline = require('readline');  
+const rl = readline.createInterface({ 
+    input: process.stdin, 
+    output: process.stdout
+});
  
 //Tarea: valor booleano y description 
 let  taskList = []; 
@@ -13,19 +17,73 @@ function printTaskList(taskList){
     for (let i = 0; i < taskList.length; ++i) { 
         if (taskList[i].done) {
             //tarea realizada 
-            console.log('[x] ' + taskList[i].description);
+            console.log((i + 1) + '. [x] ' + taskList[i].description);
         }else{ 
             //tarea no realizada 
-            console.log('[ ] ' + taskList[i].description);
+            console.log((i + 1) + '. [ ] ' + taskList[i].description);
         }
     }
      
 
-} 
- 
-addTask(taskList, 'Sacar la basura');  
-addTask(taskList, 'Lavar los platos'); 
- 
-printTaskList(taskList);
+}  
 
 // Primer modo: lectura de tareas necesarias 
+function mode1(taskList){ 
+    rl.question('Introduce una nueva tarea (fin si terminas)', function(taskDesc) { 
+        switch (taskDesc) { 
+            case 'fin': 
+                console.log('No se introducen ya más tareas'); 
+                mode2(taskList);
+                break; 
+            case 'exit': 
+              rl.close(); 
+              break;
+            default:
+                addTask(taskList, taskDesc);  
+                console.log('La lista de tareas actual es:'); 
+                printTaskList(taskList); 
+                mode1(taskList);
+                break;
+        }  
+  
+    });
+} 
+ 
+function markTaskAsDone(taskList, taskNumber){  
+    if (taskNumber >= 0 && taskNumber < taskList.length) {  
+        taskList[taskNumber].done = true;
+    } else  {
+        console.log('Invalid task number');
+    }
+}  
+ 
+function checkAllDone(taskList) { 
+    for (let task of taskList) { 
+        if (!task.done) return false;
+    }   
+    return true;  
+} 
+
+function mode2(taskList){  
+    printTaskList(taskList);
+    rl.question('Introduce el número de la tarea que has realizado (1 - N)', function(taskNumber) { 
+        switch (taskNumber) { 
+            case 'fin': 
+            case 'exit':  
+            console.log('Bye bye!');
+              rl.close(); 
+              break;
+            default:
+                markTaskAsDone(taskList, taskNumber - 1);  
+                if (checkAllDone(taskList)) { 
+                    console.log('¡Enhorabuena! Has completado todas las tareas');
+                    rl.close();
+                } else { 
+                    mode2(taskList);
+                }
+        }  
+  
+    });
+} 
+
+mode1(taskList);
